9f9b1b87aa6988d14fc1c02680bdc8dd97fa9dbf
venkatraj <venkat@codinggeek.com>
2022-07-19 11:35:55 +0530

    plugin header

---
+++ src/plugwork.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: PlugWork
+ * Plugin URI: https://tamildeveloper.com
+ * Description: The best WordPress plugin framework.
+ * Version: 1.0.0
+ * Requires at least: 6
+ * Requires PHP: 8.1
+ * Author: Venkat Raj. N.
+ * Author URI: https://tamildeveloper.com
+ * Licence: GPLv2 or later
+ *
+ * Text Domain: plugwork
+ * Domain Path: languages
+ *
+ * Copyright 2022
+ * */
+
+defined( 'ABSPATH' ) || exit;

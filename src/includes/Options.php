70d7e7857b38a8dba9d6bfe7dff984fc191337a4
venkatraj <venkat@codinggeek.com>
2022-07-20 08:48:04 +0530

    wishlist methods for Options class

--- src/includes/Options.php
+++ src/includes/Options.php
@@ -1,9 +1,31 @@
 <?php
+
 namespace Venkatraj\Plugwork;
 
 defined( 'ABSPATH' ) || exit;
 
-final class Options
-{
+final class Options {
+	public function get() {
 
+	}
+
+	public function add() {
+
+	}
+
+	public function update() {
+
+	}
+
+	public function delete() {
+
+	}
+
+	public function getAll() {
+
+	}
+
+	public function reset() {
+
+	}
 }
